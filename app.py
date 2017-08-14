a827097393189ae01ebd44e0b2553921abd6dfee
amwoods3 <amwoods3@cougars.ccis.edu>
2017-08-15 00:27:08 +0900

    Update Index to be able to display in either Japanese or English

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import json
 app = Flask(__name__)
+TEMPLATES_AUTO_RELOAD = True
 
 @app.route("/")
 
 def main():
-	return render_template("index.html")
+    with open("index_project_list") as pl:
+        project_list = json.load(pl)
+    return render_template("index.html", project_list=project_list, language="English")
 	
 	
 if __name__ == '__main__':
