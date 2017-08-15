5600cdc119d86d7d468c20ef2a2ecda6bf915757
amwoods3 <amwoods3@cougars.ccis.edu>
2017-08-15 13:27:38 +0900

    Generalize templates to have a parent template

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ app = Flask(__name__)
 TEMPLATES_AUTO_RELOAD = True
 
 @app.route("/")
-
+@app.route("/index")
 def main():
     with open("index_project_list") as pl:
         project_list = json.load(pl)
     return render_template("index.html", project_list=project_list, language="English")
-	
+
+@app.route("/deviance")
+def deviance():
+    return render_template("deviance.html")
 	
 if __name__ == '__main__':
 	app.run()
